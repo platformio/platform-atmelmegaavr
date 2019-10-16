38643a257b0c987f179149593e1de9acb1a56896
valeros <valeros@bigmir.net>
2019-10-16 13:47:03 +0300

    Initial support for atmelmegaavr platform

---
+++ builder/frameworks/_bare.py
@@ -0,0 +1,66 @@
+# Copyright 2019-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Default flags for bare-metal programming (without any framework layers)
+#
+
+from SCons.Script import Import
+
+Import("env")
+
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CFLAGS=[
+        "-std=gnu11",
+        "-fno-fat-lto-objects"
+    ],
+
+    CCFLAGS=[
+        "-Os",
+        "-w",
+        "-ffunction-sections",
+        "-fdata-sections",
+        "-flto",
+        "-mmcu=$BOARD_MCU"
+    ],
+
+    CPPDEFINES=[
+        ("F_CPU", "$BOARD_F_CPU")
+    ],
+
+    CXXFLAGS=[
+        "-std=gnu++11",
+        "-fno-exceptions",
+        "-fno-threadsafe-statics",
+        "-fpermissive",
+        "-Wno-error=narrowing"
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-flto",
+        "-mmcu=$BOARD_MCU",
+        "-Wl,--gc-sections",
+        "-Wl,--section-start=.text=%s" % env.BoardConfig().get(
+            "build.text_section_start", "0x0"),
+        "-fuse-linker-plugin"
+    ],
+
+    LIBS=["m"]
+)
+
+# copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
+env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
---
+++ builder/frameworks/arduino.py
@@ -0,0 +1,122 @@
+# Copyright 2019-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Arduino
+
+Arduino Wiring-based Framework allows writing cross-platform software to
+control devices attached to a wide range of Arduino boards to create all
+kinds of creative coding, interactive objects, spaces or physical experiences.
+
+http://arduino.cc/en/Reference/HomePage
+"""
+
+import sys
+from os.path import isdir, isfile, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-arduino-megaavr")
+assert isdir(FRAMEWORK_DIR)
+
+board = env.BoardConfig()
+build_core = board.get("build.core", "")
+
+CPPDEFINES = [
+    "ARDUINO_ARCH_MEGAAVR",
+    ("ARDUINO", 10808)
+]
+
+if "build.usb_product" in board:
+    CPPDEFINES += [
+        ("USB_VID", board.get("build.hwids")[0][0]),
+        ("USB_PID", board.get("build.hwids")[0][1]),
+        ("USB_PRODUCT", '\\"%s\\"' %
+         board.get("build.usb_product", "").replace('"', "")),
+        ("USB_MANUFACTURER", '\\"%s\\"' %
+         board.get("vendor", "").replace('"', ""))
+    ]
+
+env.SConscript("_bare.py", exports="env")
+
+env.Append(
+    CPPDEFINES=CPPDEFINES,
+
+    CPPPATH=[
+        join(FRAMEWORK_DIR, "cores", build_core, "api", "deprecated"),
+        join(FRAMEWORK_DIR, "cores", build_core)
+    ],
+
+    LIBSOURCE_DIRS=[
+        join(FRAMEWORK_DIR, "libraries")
+    ]
+)
+
+# Bootloader and fuses for uploading purposes
+bootloader_config = board.get("bootloader", {})
+if "BOOTLOADER_CMD" not in env:
+    if env.subst("$BOARD") == "uno2018":
+        bootloader_path = join(
+            FRAMEWORK_DIR, "bootloaders", board.get("bootloader.file", ""))
+        if isfile(bootloader_path):
+            env.Replace(BOOTLOADER_CMD='-Uflash:w:"%s":i' % bootloader_path)
+        else:
+            sys.stderr.write(
+                "Error: Couldn't find bootloader image %s\n" % bootloader_path)
+            env.Exit(1)
+
+if "FUSES_CMD" not in env:
+    for fuse in ("OSCCFG", "SYSCFG0", "BOOTEND"):
+        if not bootloader_config.get(fuse, ""):
+            sys.stderr.write("Error: Missing %s fuse value\n" % fuse)
+            env.Exit(1)
+
+    env.Replace(
+        FUSES_CMD="-Ufuse2:w:%s:m -Ufuse5:w:%s:m -Ufuse8:w:%s:m" % (
+            bootloader_config.get("OSCCFG"),
+            bootloader_config.get("SYSCFG0"),
+            bootloader_config.get("BOOTEND")
+        )
+    )
+
+#
+# Target: Build Core Library
+#
+
+libs = []
+
+if "build.variant" in board:
+    variants_dir = join(
+        "$PROJECT_DIR", board.get("build.variants_dir")) if board.get(
+            "build.variants_dir", "") else join(FRAMEWORK_DIR, "variants")
+
+    env.Append(
+        CPPPATH=[
+            join(variants_dir, board.get("build.variant"))
+        ]
+    )
+    libs.append(env.BuildLibrary(
+        join("$BUILD_DIR", "FrameworkArduinoVariant"),
+        join(variants_dir, board.get("build.variant"))
+    ))
+
+libs.append(env.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkArduino"),
+    join(FRAMEWORK_DIR, "cores", build_core)
+))
+
+env.Prepend(LIBS=libs)
